fb05e251afd051a111cd4e4019e8c2f81c529913
Gonzalo Gazzero <gonzagazzero2017@gmail.com>
2024-05-21 23:14:19 -0300

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { login } from "../config/firebase";
+
+const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log("submit")
+        try {
+            const credentialUser = await login({ email, password });
+            console.log(credentialUser);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Ingrese email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Ingrese constraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
+};
+
+export default Login;
